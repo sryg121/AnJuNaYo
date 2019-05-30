8081cc057c34d3bb44d36ba7a69be1e6e55048e6
dkdlzb725 <dkdlzb725@gmail.com>
2019-05-30 18:28:31 +0900

    현재 날씨를 알아내는 함수 구현

--- project_softwarestudio_anjunayo/project_softwarestudio_anjunayo/weatherRecommend/RecommendVC.swift
+++ project_softwarestudio_anjunayo/project_softwarestudio_anjunayo/weatherRecommend/RecommendVC.swift
@@ -8,6 +8,67 @@
 
 import UIKit
 
+//날씨 상태 표현하는 string을 return하는 함수
+func findWeather() -> String {
+    var weatherCode: Int = 100
+    var weatherStatus: String = ""
+    let url = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=728cdb1629d7ea4c4c04235e81643675"
+    let apiURL: URL! = URL(string: url)
+    
+    let apidata = try! Data(contentsOf: apiURL)
+    do{
+        let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+        let weatherArray = apiDictionary["weather"] as! NSArray
+        let weatherDictionary = weatherArray[0] as! NSDictionary
+        if let weatherID = weatherDictionary["id"] as? Int {
+            weatherCode = weatherID
+        } else {
+            print("error")
+        }
+    } catch {
+        print("error")
+    }
+    
+    //        200 - 232 번개
+    //        300 - 531 비
+    //        600 - 622 눈
+    //        700, 721,741 안개
+    //        711 스모크
+    //        733 751 761 먼지 심함
+    //        762 화산재
+    //        771 돌풍
+    //        781 토네이도
+    //        800 맑음
+    //        801 - 804 흐림
+    
+    if weatherCode >= 200, weatherCode <= 232 {
+        weatherStatus = "번개"
+    }else if weatherCode >= 300, weatherCode <= 531 {
+        weatherStatus = "비"
+    }else if weatherCode >= 600, weatherCode <= 622 {
+        weatherStatus = "눈"
+    }else if weatherCode == 700 || weatherCode == 721 || weatherCode==741 {
+        weatherStatus = "안개"
+    }else if weatherCode == 711 {
+        weatherStatus = "스모크"
+    }else if weatherCode == 733 || weatherCode == 751 || weatherCode == 761 {
+        weatherStatus = "먼지 심함"
+    }else if weatherCode == 762 {
+        weatherStatus = "화산재"
+    }else if weatherCode == 771 {
+        weatherStatus = "돌풍"
+    }else if weatherCode == 781 {
+        weatherStatus = "토네이도"
+    }else if weatherCode == 800 {
+        weatherStatus = "맑음"
+    }else if weatherCode >= 801, weatherCode <= 804 {
+        weatherStatus = "흐림"
+    }
+    
+    return weatherStatus
+    
+}
+
 class RecommendVC: UIViewController {
 
     @IBOutlet weak var recommendView: UICollectionView!
@@ -15,6 +76,8 @@ class RecommendVC: UIViewController {
     @IBOutlet weak var weatherView: UITableView!
     
     var recommendList: [Recommend] = []
+    
+    var weatherStr: String = ""
     
     override func viewDidLoad() {
         
@@ -31,11 +94,7 @@ class RecommendVC: UIViewController {
         imageView.image = image
         navigationItem.titleView = imageView
         
-        // 날씨 데이터 나타내기
-        WeatherDatasource.shared.fetchSummary(lat: 37.498206, lon: 127.02761) {
-            [weak self] in
-            self?.weatherView.reloadData()
-        }
+        
         
     }
 }
