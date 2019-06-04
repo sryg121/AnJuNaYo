7b7da27cc201eff67125ec44f0cc2f1f57c815ab
JaeeunAhn <49272528+JaeeunAhn@users.noreply.github.com>
2019-06-04 17:04:55 +0900

    weather api

---
+++ weather/weather/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  weather
+//
+//  Created by 안재은 on 03/06/2019.
+//  Copyright © 2019 SwiftHelloWorld. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "2c08b3f7-61e6-4c48-bb1c-92a728633c26"
---
+++ weather/weather/Prototype/summaryapi.playground/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  
+//
+//  Created by 안재은 on 03/06/2019.
+//
+
+import Foundation
